81f97ea79733d543a70263e750a7ba2cb1167fcd
EmranHoque <emran.cse.cu@gmail.com>
2026-03-28 22:43:07 +0600

    Add debug logging for CORS_ALLOWED_ORIGINS

--- curova_backend/settings.py
+++ curova_backend/settings.py
@@ -131,6 +131,7 @@ CORS_ALLOWED_ORIGINS = [
         "CORS_ALLOWED_ORIGINS", default="http://localhost:5173,http://127.0.0.1:5173"
     ).split(",")
 ]
+print(f"DEBUG: CORS_ALLOWED_ORIGINS = {CORS_ALLOWED_ORIGINS}")
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
     "accept",
