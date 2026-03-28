f669711306c5d90888facf13156297c88c07f8c5
EmranHoque <emran.cse.cu@gmail.com>
2026-03-28 22:40:50 +0600

    Add explicit CORS headers configuration

--- curova_backend/settings.py
+++ curova_backend/settings.py
@@ -132,6 +132,29 @@ CORS_ALLOWED_ORIGINS = [
     ).split(",")
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_EXPOSE_HEADERS = [
+    "Content-Type",
+    "X-CSRFToken",
+]
 
 # Internationalization
 LANGUAGE_CODE = "en-us"
